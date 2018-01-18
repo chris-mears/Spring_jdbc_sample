08ef5170e91618508f3085d786b2f8926d46d749
AMER\CXM5Z0V <Chris_Mears@homedepot.com>
2018-01-18 17:49:10 -0500

    updated project still trying to connect to aws db

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -3,12 +3,18 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
 
 @Repository("rideRepository")
 public class RideRepositoryImpl implements RideRepository {
+
+	//This is how the repository connects to the db
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public List<Ride> getRides() {
